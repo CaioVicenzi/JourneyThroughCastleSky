911619fa7533d0d5ad21ee47caee4c4fa0d91e71
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-09-11 08:32:35 -0300

    Initial Commit

---
+++ JourneyThroughCastleSky/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  JourneyThroughCastleSky
+//
+//  Created by Caio Marques on 11/09/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ JourneyThroughCastleSky/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  JourneyThroughCastleSky
+//
+//  Created by Caio Marques on 11/09/24.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
