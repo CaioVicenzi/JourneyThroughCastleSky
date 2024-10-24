a58792577dc5151f8812769589dfb1d2ff779b81
Victor Inácio <42353254+victor-inacio@users.noreply.github.com>
2024-10-24 16:48:18 -0300

    Classe Door movida para outro arquivo

--- JourneyThroughCastleSky/Scene/TopDownScene/TopDownScene.swift
+++ JourneyThroughCastleSky/Scene/TopDownScene/TopDownScene.swift
@@ -8,72 +8,7 @@ import Foundation
 import SpriteKit
 
 
-class Door: SKSpriteNode {
-    
-    var enabled = true
-    
-    var destiny: String {
-        return self.userData?.value(forKey: "destiny") as! String
-    }
-    
-    var spawnDirection: CGVector {
-        let directionString = self.userData?.value(forKey: "spawnDirection") as! String
-        
-        switch(directionString) {
-            case "up":
-                return .init(dx: 0, dy: 1)
-            case "left":
-                return .init(dx: -1, dy: 0)
-            case "right":
-                return .init(dx: 1, dy: 0)
-            case "down":
-                return .init(dx: 0, dy: -1)
-            default:
-                return .init(dx: 0, dy: 1)
-        }
-    }
-    
-    var destintyDoorName: String {
-        return self.userData?.value(forKey: "doorDestiny") as! String
-    }
-    
-    var destinyPhase: GamePhase? {
-        return GamePhase(rawValue: self.destiny)
-    }
-    
-    public func handleNodeContact(node: SKNode) {
-        if (!enabled) {
-            return
-        }
-        
-        
-        if node == User.singleton.spriteComponent.sprite {
-            
-            let nodeScene = node.scene as! TopDownScene
-            
-            if let gamePhase = destinyPhase {
-                nodeScene
-                    .goNextScene(
-                        gamePhase,
-                        destinyDoorName: self.destintyDoorName
-                    )
-            }
-            
-        }
-    }
-    
-    public func enable() {
-        
-        enabled = true
-        
-    }
-    
-    public func disable() {
-        enabled = false
-    }
-    
-    
-}
+
 
 /// Classe genérica que vai conter todos os atributos e métodos que são compartilhados por todas as cenas que contém o modo TopDown de exploração de ambiente.
 /// - parameters
