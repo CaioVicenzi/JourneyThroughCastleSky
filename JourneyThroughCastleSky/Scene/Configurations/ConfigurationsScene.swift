e9389631a4eb6e2d97175a3ccdb59d8d1e51f9b3
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-10-28 16:28:25 -0300

    Adding positions...

---
+++ JourneyThroughCastleSky/Scene/Configurations/ConfigurationsScene.swift
@@ -0,0 +1,16 @@
+//
+//  ConfigurationsScene.swift
+//  JourneyThroughCastleSky
+//
+//  Created by João Ângelo on 28/10/24.
+//
+
+import Foundation
+import SpriteKit
+
+class ConfigurationsScene: SKScene{
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .red
+    }
+}
