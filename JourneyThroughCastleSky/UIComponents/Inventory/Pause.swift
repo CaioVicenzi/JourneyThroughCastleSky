d706357f71f02994aa8b61cce6c201dd9228b9a7
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-11-14 17:57:54 -0300

    feat/creditos

--- JourneyThroughCastleSky/UIComponents/Inventory/Pause.swift
+++ JourneyThroughCastleSky/UIComponents/Inventory/Pause.swift
@@ -207,7 +207,9 @@ class Pause {
             }
             
             if isEnterKey {
-                self.pauseState = .ITEM_SELECTION
+                if (inventoryCategorySelected == 1 && consumableExists()) || (inventoryItemSelected == 2 && acervoExists()) {
+                    self.pauseState = .ITEM_SELECTION
+                }
             }
             
             if isEscKey {
@@ -220,6 +222,28 @@ class Pause {
         else if self.pauseState == .ITEM_SELECTION {
             inputItems(keyCode)
         }
+    }
+    
+    // verifica se existe um item consumível dentro do inventário do usuário
+    private func consumableExists () -> Bool {
+        for item in User.singleton.inventoryComponent.itens {
+            if item.consumableComponent?.effect.type != .NONE {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    // verifica se existe um item no acervo do usuário, retorna true se existe e false se não existe.
+    private func acervoExists () -> Bool {
+        for item in User.singleton.inventoryComponent.itens {
+            if item.consumableComponent?.effect.type == .NONE {
+                return true
+            }
+        }
+        
+        return false
     }
     
     private func titleScreenButtonPressed() {
@@ -325,11 +349,71 @@ class Pause {
     private func setupCredits () {
         creditsBackground = SKShapeNode(rectOf: CGSize(width: gameScene.size.width / 1.2, height: gameScene.size.height / 1.8))
         creditsBackground?.position = .zero
-        creditsBackground?.fillColor = .blue
+        creditsBackground?.fillColor = .clear
+        creditsBackground?.strokeColor = .clear
         creditsBackground?.position.y -= 50
         if let creditsBackground {
             pause?.addChild(creditsBackground)
         }
+        
+        let creditsBackground2 = SKSpriteNode(imageNamed: "itemsInventory")
+        creditsBackground2.position = .zero
+        creditsBackground2.setScale(0.6)
+        creditsBackground?.addChild(creditsBackground2)
+        
+        let creditsTitle = SKLabelNode()
+        creditsTitle.text = "Créditos"
+        creditsTitle.position = .zero
+        creditsTitle.fontName = "Lora-Medium"
+        creditsTitle.position.y += creditsBackground2.frame.height / 1.9
+        creditsTitle.fontSize = 72
+        creditsTitle.zPosition += 1
+        creditsBackground2.addChild(creditsTitle)
+        
+        let developersTitle = SKLabelNode(text: "Developers")
+        developersTitle.fontName = "Lora-Bold"
+        developersTitle.position = .zero
+        developersTitle.position.y += creditsBackground2.frame.height / 4
+        developersTitle.position.x -= creditsBackground2.frame.width / 2
+        developersTitle.zPosition += 1
+        creditsBackground2.addChild(developersTitle)
+        
+        let developers = SKLabelNode()
+        developers.text = """
+            Caio Marques
+            João Ângelo
+            Chus
+        """
+        developers.position = .zero
+        developers.numberOfLines = 0
+        developers.fontName = "Lora-Medium"
+        developers.position.x -= creditsBackground2.frame.width / 2.2
+        //developers.position.y -= creditsBackground2.frame.height / 6
+        developers.zPosition += 1
+        developers.fontSize = 20
+        creditsBackground2.addChild(developers)
+        
+        let designersTitle = SKLabelNode()
+        designersTitle.text = "Designers"
+        designersTitle.fontName = "Lora-Bold"
+        designersTitle.position = .zero
+        designersTitle.position.y += creditsBackground2.frame.height / 4
+        designersTitle.position.x += creditsBackground2.frame.width / 3
+        designersTitle.zPosition += 1
+        creditsBackground2.addChild(designersTitle)
+
+        let designers = SKLabelNode()
+        designers.text = "Vanessa nessa\nSamuel Lima"
+        designers.position = .zero
+        designers.numberOfLines = 0
+        designers.fontName = "Lora-Medium"
+        designers.position.x += creditsBackground2.frame.width / 2.8
+        //designers.position.y -= creditsBackground2.frame.height / 4
+        designers.zPosition += 1
+        designers.fontSize = 20
+        creditsBackground2.addChild(designers)
+        
+        
         
         creditsBackground?.isHidden  = true
     }
